9d236b23fa28ee47b5904775257c01808bab8fa9
rushabhT3 <rushabhtrivedi03@gmail.com>
2023-03-18 03:15:52 +0530

    Add+Del Func and Edit button only

--- EditDelFunc.js
+++ EditDelFunc.js
@@ -5,12 +5,14 @@ form.addEventListener("submit", addItem);
 // Delete event
 itemList.addEventListener("click", removeItem);
 
+// e is the event parameter
 function addItem(e){
     e.preventDefault();
 
-    var newItem = document.querySelector("#item").value;
+    // make input lo ek new id diye #item and usko utha liye
+    var newItem = document.getElementById("item").value;
 
-    // create new li element
+    // create new li element and add classname and value of the newItem inside it
     var li = document.createElement("li");
     li.className = "list-group-item";
     li.appendChild(document.createTextNode(newItem));
@@ -20,7 +22,7 @@ function addItem(e){
     deleteBtn.className = "btn btn-danger btn-sm float-right delete";
     deleteBtn.appendChild(document.createTextNode('x'));
 
-    // append button to li
+    // append button to li bcz the delete button is inside of li
     li.appendChild(deleteBtn);
 
     itemList.appendChild(li);
@@ -31,6 +33,7 @@ function removeItem(e){
     if(e.target.classList.contains("delete")){
         if(confirm("are you sure?")){
             var li = e.target.parentElement;
+            // parentElement of the red cross is the element itself
             itemList.removeChild(li);
         }
     }
