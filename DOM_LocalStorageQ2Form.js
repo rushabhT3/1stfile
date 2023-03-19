c6188d9e048e098c8d010eb576ac54819cc03703
rushabhT3 <rushabhtrivedi03@gmail.com>
2023-03-19 14:05:55 +0530

    Storing the data in object form

--- DOM_LocalStorageQ2Form.js
+++ DOM_LocalStorageQ2Form.js
@@ -1,8 +1,12 @@
 function onsignup(event){
-    // prevent default, make variables that store the values on submission, save those values inside the local storage
+    // prevent default, make variables that store the values on submission
+    // save those values inside the local storage by localStorage.setItem("key", "value") format
     
     event.preventDefault();
 
+    // target k baad name wala bhi use kr sakte the id k jagah
+    // event.target. .value wale me submit k baad ki values select; 
+    // with getElementBy wale me html tags pick hote
     const name = event.target.myName.value;
     const email = event.target.exampleInputEmail1.value;
     const tel = event.target.tel.value;
@@ -10,4 +14,25 @@ function onsignup(event){
     localStorage.setItem("Name", name);
     localStorage.setItem("Email", email);
     localStorage.setItem("Phone Number", tel);
+
+    // declaring an object instead of the separate variables for name, email and phone number
+    // ye object directly store nahi kr sakte 
+    let myObj = {
+        "Name" : name,
+        "Email" : email,
+        "Phone Number" : tel,
+    }
+
+    // to convert an object into a string; myObj_serialized is a string
+    let myObj_serialized = JSON.stringify(myObj);
+
+    // storing as earlier but This is the local storage in the form of an Object
+    localStorage.setItem("myObj", myObj_serialized);
+
+    // to convert string back to the object; myObj_deserialized is an object
+    let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));
+
+    console.log(myObj_serialized);
+    console.log(myObj_deserialized);
+    
 }
