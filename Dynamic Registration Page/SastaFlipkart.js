9aeca8302d86611c6720fd484ec83c5abf63369c
rushabhT3 <rushabhtrivedi03@gmail.com>
2023-04-07 13:41:19 +0530

    minor change now working properly

--- Dynamic Registration Page/SastaFlipkart.js
+++ Dynamic Registration Page/SastaFlipkart.js
@@ -12,16 +12,17 @@ function onSignUp(event) {
     }
 
     if (price && product && category) {
-        axios.post(`https://crudcrud.com/api/64aad991ca00405fb318d2d2983e058e/SastaFlipkart`, myObj)
-            .then(() => {
-                onScreenFunction(myObj);
+        axios.post(`https://crudcrud.com/api/7d7ac4af2b804c0cba235fd207c256d9/SastaFlipkart`, myObj)
+            .then((response) => {
+                const newObj = response.data; // use the response to get the entire object
+                onScreenFunction(newObj);
             })
     } else {
         alert('Enter All the Details')
     }
 }
 document.addEventListener('DOMContentLoaded', () => {
-    axios.get(`https://crudcrud.com/api/64aad991ca00405fb318d2d2983e058e/SastaFlipkart`)
+    axios.get(`https://crudcrud.com/api/7d7ac4af2b804c0cba235fd207c256d9/SastaFlipkart`)
         .then((resolve) => {
             resolve.data.forEach(element => {
                 onScreenFunction(element);
@@ -39,11 +40,22 @@ function onScreenFunction(myObj) {
     const li = document.createElement('li');
     li.innerHTML = `<small>${myObj.price} - ${myObj.product} - ${myObj.category}</small>`;
 
+
+    if (myObj.category == 'Electronics') {
+        ulElectronics.appendChild(li);
+    }
+    if (myObj.category == 'Food') {
+        ulFood.appendChild(li);
+    }
+    if (myObj.category == 'Body Care') {
+        ulSkinCare.appendChild(li);
+    }
+
     const delBtn = document.createElement('input');
     delBtn.type = 'button';
     delBtn.value = 'Delete Order';
     delBtn.onclick = () => {
-        axios.delete(`https://crudcrud.com/api/64aad991ca00405fb318d2d2983e058e/SastaFlipkart/${myObj._id}`)
+        axios.delete(`https://crudcrud.com/api/7d7ac4af2b804c0cba235fd207c256d9/SastaFlipkart/${myObj._id}`)
             .then(() => {
                 if (myObj.category == 'Electronics') {
                     ulElectronics.removeChild(li);
@@ -55,19 +67,8 @@ function onScreenFunction(myObj) {
                     ulSkinCare.removeChild(li);
                 }
             })
-            .catch((err) => {
-                console.error(err);
-            })
+
     }
 
     li.appendChild(delBtn);
-    if (myObj.category == 'Electronics') {
-        ulElectronics.appendChild(li);
-    }
-    if (myObj.category == 'Food') {
-        ulFood.appendChild(li);
-    }
-    if (myObj.category == 'Body Care') {
-        ulSkinCare.appendChild(li);
-    }
 }
